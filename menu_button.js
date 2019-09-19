29b33d6becb93e8df1a8dba6289513f1a87da5f0
Alexis Blanchard <alexis.blanchard0@gmail.com>
2019-09-19 18:39:28 +0200

    menu_button.js empty

--- menu_button.js
+++
@@ -1,9 +0,0 @@
-var display_blue = document.getElementById("lego_blue")
-var display_red = document.getElementById("lego_red")
-display_red.style.display = "none"
-
-function switch_button()
-{
-    display_blue.style.display = "none";
-    display_red.style.display = "flex";
-}
