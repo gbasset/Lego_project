36d1d2dd76d93eed3bbf9aa3f8e55be8372b3b8a
Clément Héré <clement.here.tech@gmail.com>
2019-10-03 15:26:30 +0200

    final version of homepge css html and js

--- homepage.js
+++ homepage.js
@@ -1,6 +1,9 @@
 const display_lego_red = document.getElementById("lego_red")
 const display_lego_blue = document.getElementById("lego_blue")
 const display_hamburger = document.getElementById("hamburger")
+
+display_lego_red.style.display = "none";
+display_hamburger.style.display = "none";
     
 lego_blue.addEventListener('click', () => {
     display_hamburger.style.display = "flex";
@@ -11,7 +14,7 @@ lego_blue.addEventListener('click', () => {
 
 lego_red.addEventListener('click', () => {
     display_lego_blue.style.display = "flex";
-    display_hamburger.style.display = "none";
     display_lego_red.style.display = "none";
+    display_hamburger.style.display = "none";
 })
 
